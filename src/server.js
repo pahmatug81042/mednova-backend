9d75c1f109ac91ab2beb7e94910adb4d4e55fcdc
Parsa Ahmadi Nasab Emran <parsatempleuniversity@gmail.com>
2025-10-24 09:39:07 -0400

    Configure server startup with database connection

---
+++ src/server.js
@@ -0,0 +1,19 @@
+import mongoose from "mongoose";
+import app from "./app";
+
+const PORT = process.env.PORT || 8000;
+const MONGO_URI = process.env.MONGO_URI;
+
+// Connect to MongoDB
+mongoose.connect(MONGO_URI)
+    .then (() => {
+        console.log("Connected to MongoDB");
+
+        app.listen(PORT, () => {
+            console.log(`MedNova Backend running on port ${PORT}`);
+        });
+    })
+    .catch((error) => {
+        console.error("MongoDB Connection Failed", error);
+        process.exit(1);
+    });
